e34cc5f2f27572929cebc41c716aa9504cf26fff
alu0101206011 <alu0101206011@ull.edu.es>
2022-05-06 19:18:43 +0100

    Secuential mode using a total data buffer

--- main.cpp
+++ main.cpp
@@ -3,54 +3,42 @@
 #include <iostream>
 #include <math.h>
 
-class Statistics {
-    // https://stackoverflow.com/questions/7988486/how-do-you-calculate-the-variance-median-and-standard-deviation-in-c-or-java/7988556#7988556
-    QVector<float> data;
-    int size;
+const unsigned TotalData = 60*60*24*365;
+const unsigned SampleSize = 60*60*24;
+QVector<float> Buffer;
 
-public:
-    Statistics(QVector<float> data) {
-        this->data = data;
-        size = data.length();
-    }
 
-    double getMean() {
-        double sum = 0.0;
-        for(double a : data)
-            sum += a;
-        return sum/size;
-    }
+double getMean(QVector<float>* data, unsigned start_pos, unsigned sample_size) {
+    double sum = 0.0;
+    for (int i = start_pos; i < start_pos + sample_size; i++)
+        sum += (*data)[i];
+    return sum/sample_size;
+}
 
-    double median() {
-       //Arrays.sort(data);
-        std::sort(data.begin(),data.end());
-       if (data.length() % 2 == 0)
-          return (data[(data.length() / 2) - 1] + data[data.length() / 2]) / 2.0;
-       return data[data.length() / 2];
-    }
-};
 
-int main(int argc, char *argv[])
-{
+double median(QVector<float>* data, unsigned start_pos, unsigned sample_size) {
+   std::sort((*data).begin() + start_pos, (*data).begin() + start_pos + sample_size - 1);
+   if ((start_pos + sample_size - 1) % 2 == 0)
+      return ((*data)[((start_pos + sample_size - 1) / 2) - 1] + (*data)[(start_pos + sample_size - 1) / 2]) / 2.0;
+   return (*data)[(start_pos + sample_size - 1) / 2];
+}
+
+int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
+    Buffer.resize(TotalData);
 
-    QVector<float> d;
-    unsigned int total = 60*60*24*365; // sec * min * hour * days
-    for(unsigned int i =0; i < total;i++)
-    {
-        d.push_back(random() % 50 + 50);
-        if(i % (24*3600) == 0)
-        {
-            Statistics s(d);
-            s.getMean();
-            s.median();
-            std::cout << i << " of " << total << " ";
-            std::cout << "average temperature " << s.getMean() << " ";
-            std::cout << " with median " << s.median() << "\n";
-            d.erase(d.begin(),d.end());
+    auto start1 = std::chrono::high_resolution_clock::now();
+    for(unsigned int i = 0; i < TotalData; i++) {
+        Buffer[i] = random() % 50 + 50;
+        if(i % SampleSize == 0 && i != 0) {
+            std::cout << i << " of " << TotalData << " ";
+            std::cout << "average temperature " << getMean(&Buffer, i - SampleSize, SampleSize) << " ";
+            std::cout << " with median " << median(&Buffer, i - SampleSize, SampleSize) << "\n";
         }
     }
-
+    auto stop1 = std::chrono::high_resolution_clock::now();
+    auto duration1 = std::chrono::duration_cast<std::chrono::microseconds>(stop1 - start1);
+    std::cout << duration1.count() << "\n";
     std::cout << "Done in serial mode\n";
 
     return 0;
