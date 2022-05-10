2417dec07ef6baca00cd8f7487985af4fce812d9
alu0101278919 <alu0101278919@ull.edu.es>
2022-05-10 11:18:21 +0100

    QThreadpool working with default maxThread.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <QCoreApplication>
 #include <QVector>
 #include <iostream>
+#include <QRunnable>
+#include <QThreadPool>
 #include <math.h>
 
 const unsigned TotalData = 60*60*24*365;
@@ -23,23 +25,61 @@ double median(QVector<float>* data, unsigned start_pos, unsigned sample_size) {
    return (*data)[(start_pos + sample_size - 1) / 2];
 }
 
+class DayTemp : public QRunnable
+{
+private:
+    QVector<float>* data_;
+    unsigned start_pos_;
+    unsigned sample_size_;
+
+public:
+    DayTemp(QVector<float>* data, unsigned start_pos, unsigned sample_size) {
+        data_ = data;
+        start_pos_ = start_pos;
+        sample_size_ = sample_size;
+    }
+
+    void run()
+    {
+        std::cout << start_pos_ << " of " << TotalData << " ";
+        std::cout << "average temperature " << getMean(&Buffer, start_pos_, SampleSize) << " ";
+        std::cout << " with median " << median(&Buffer, start_pos_, SampleSize) << "\n";
+    }
+
+    QVector<float>* get_data() { return data_; }
+    unsigned get_start_pos() { return start_pos_; }
+    unsigned get_sample_size() { return sample_size_; }
+
+    void set_data(QVector<float>* datos) { data_ = datos; }
+    void set_start_pos(unsigned start_pos) { start_pos_ = start_pos; }
+    void set_sample_size(unsigned sample_size) { sample_size_ = sample_size; }
+};
+
 int main(int argc, char *argv[]) {
-    QCoreApplication a(argc, argv);
     Buffer.resize(TotalData);
 
-    auto start1 = std::chrono::high_resolution_clock::now();
     for(unsigned int i = 0; i < TotalData; i++) {
-        Buffer[i] = random() % 50 + 50;
-        if(i % SampleSize == 0 && i != 0) {
-            std::cout << i << " of " << TotalData << " ";
-            std::cout << "average temperature " << getMean(&Buffer, i - SampleSize, SampleSize) << " ";
-            std::cout << " with median " << median(&Buffer, i - SampleSize, SampleSize) << "\n";
-        }
+        Buffer[i] = rand() % 50 + 50;
     }
+
+    auto start1 = std::chrono::high_resolution_clock::now();
+
+    for(unsigned int i = 0; i < TotalData; i += SampleSize)
+    {
+        DayTemp *t1 = new DayTemp(&Buffer, i, SampleSize);
+    //TestTask *t2 = new TestTask;
+        QThreadPool::globalInstance()->start(t1);
+    //QThreadPool::globalInstance()->start(t2);
+    }
+
+    QThreadPool::globalInstance()->waitForDone();
+
     auto stop1 = std::chrono::high_resolution_clock::now();
+
     auto duration1 = std::chrono::duration_cast<std::chrono::microseconds>(stop1 - start1);
     std::cout << duration1.count() << "\n";
-    std::cout << "Done in serial mode\n";
+    std::cout << "Done in QThreadPool mode\n";
+    std::cout << "Using " << QThreadPool::globalInstance()->maxThreadCount() << " Threads.\n";
 
     return 0;
 }
